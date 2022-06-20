f61eabfbf751ff73f433200c9998ee0a6d09108b
Grandinho <monyx5861@gmail.com>
2022-06-20 17:55:10 +0200

    Added first script

--- FirstProject/Assets/PrintToConsole.cs
+++ FirstProject/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Fockin fuck");
     }
 
     // Update is called once per frame
