e81b129565d3ee9d0049e8b047b6509864e26f14
Grandinho <monyx5861@gmail.com>
2022-07-01 21:46:43 +0200

    Added layout

--- CarGame/Assets/Collision.cs
+++ CarGame/Assets/Collision.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        void OnCollisionEnter2D(Collision2D other) 
+        {
+           Debug.Log("Lewo na rondzie"); 
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log("Finish");
+        }
 }
